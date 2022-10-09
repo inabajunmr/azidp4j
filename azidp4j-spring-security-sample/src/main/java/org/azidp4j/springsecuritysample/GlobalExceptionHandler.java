309f696615c97bfbad2d9ed22f9204db350ad697
inabajunmr <inabajun.for.regi@gmail.com>
2022-10-09 15:16:14 +0900

    add server.tomcat.relaxed-query-chars

---
+++ azidp4j-spring-security-sample/src/main/java/org/azidp4j/springsecuritysample/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package org.azidp4j.springsecuritysample;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GlobalExceptionHanlder extends ResponseEntityExceptionHandler {
+}
