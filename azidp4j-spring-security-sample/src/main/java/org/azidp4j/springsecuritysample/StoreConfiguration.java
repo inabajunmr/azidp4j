32adeeecd64c500a2db49fb2e0703ef02bd32ae7
inabajunmr <inabajun.for.regi@gmail.com>
2022-10-15 04:12:31 +0900

    wip

--- azidp4j-spring-security-sample/src/main/java/org/azidp4j/springsecuritysample/StoreConfiguration.java
+++ azidp4j-spring-security-sample/src/main/java/org/azidp4j/springsecuritysample/StoreConfiguration.java
@@ -1,29 +1,20 @@
 package org.azidp4j.springsecuritysample;
 
-import com.nimbusds.jose.jwk.JWKSet;
-import org.azidp4j.AzIdPConfig;
 import org.azidp4j.client.ClientStore;
 import org.azidp4j.client.InMemoryClientStore;
-import org.azidp4j.jwt.JWSIssuer;
 import org.azidp4j.springsecuritysample.consent.InMemoryUserConsentStore;
 import org.azidp4j.token.accesstoken.AccessTokenService;
-import org.azidp4j.token.accesstoken.jwt.JwtAccessTokenService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.azidp4j.token.accesstoken.inmemory.InMemoryAccessTokenService;
+import org.azidp4j.token.accesstoken.inmemory.InMemoryAccessTokenStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StoreConfiguration {
 
-    @Autowired JWKSet jwkSet;
-
-    @Autowired AzIdPConfig config;
-
     @Bean
-    public AccessTokenService accessTokenService(AzIdPConfig config) {
-        //        return new InMemoryAccessTokenService(new InMemoryAccessTokenStore());
-
-        return new JwtAccessTokenService(jwkSet, new JWSIssuer(jwkSet), config.issuer, () -> "123");
+    public AccessTokenService accessTokenService() {
+        return new InMemoryAccessTokenService(new InMemoryAccessTokenStore());
     }
 
     @Bean
